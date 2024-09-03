58bf13ec8bb3285c66e6109d2836ce8e87b59bea
Gokulkrish17 <gokulgokul868@gmail.com>
2024-09-03 12:25:30 +0530

    Changed Detail model

--- Register/src/main/java/com/example/demo/entity/Details.java
+++ Register/src/main/java/com/example/demo/entity/Details.java
@@ -1,79 +1,26 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "detail")
 public class Details {
-@Id
-	
-	private int studentid;
-	private String name;
-	private int age;
-	private String course;
-	private String courseDuration;
-	private String courseTiming;
-	private long phoneNumber;
-	public int getStudentid() {
-		return studentid;
-	}
-	public void setStudentid(int studentid) {
-		this.studentid = studentid;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public String getCourse() {
-		return course;
-	}
-	public void setCourse(String course) {
-		this.course = course;
-	}
-	public String getCourseDuration() {
-		return courseDuration;
-	}
-	public void setCourseDuration(String courseDuration) {
-		this.courseDuration = courseDuration;
-	}
-	public String getCourseTiming() {
-		return courseTiming;
-	}
-	public void setCourseTiming(String courseTiming) {
-		this.courseTiming = courseTiming;
-	}
-	public long getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(long phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public Details(int studentid, String name, int age, String course, String courseDuration, String courseTiming,
-			long phoneNumber) {
-		super();
-		this.studentid = studentid;
-		this.name = name;
-		this.age = age;
-		this.course = course;
-		this.courseDuration = courseDuration;
-		this.courseTiming = courseTiming;
-		this.phoneNumber = phoneNumber;
-	}
-	public Details() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-	
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "student_Id")
+	private int studentId;
+
+	@Column(name = "student_Name")
+	private String studentName;
+
+	@Column(name = "student_Hobby")
+	private String studentHobby;
 }
 	
